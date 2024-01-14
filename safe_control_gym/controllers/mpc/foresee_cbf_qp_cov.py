corrected for covariance. now produces plots

safe_control_gym/controllers/mpc/foresee_cbf_qp_cov.py
@@ -148,7 +148,7 @@ class FORESEE_CBF_QP_COV(MPC):
         # kT2y = 1.0
         # kT20 = 5.0
      
-        self.adapt = True
+        self.adapt = False
         self.num_adapt_iterations = 10
 
         self.params = np.array([ kx, kv, krx, kR, kRv ])
@@ -291,27 +291,29 @@ class FORESEE_CBF_QP_COV(MPC):
             new_weights = jnp.zeros((2*n+1,N))
 
             mu = get_mean(sigma_points, weights)
-            u = FORESEE_CBF_QP_COV.controller(params, mu.reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
+            control = FORESEE_CBF_QP_COV.controller(params, mu.reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
 
             # for i in range(N):
-            #     u = FORESEE_CBF_QP_COV.controller(params, sigma_points[:,i].reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
-            #     z = jnp.append( sigma_points[:,i].reshape(-1,1), u, axis=0 )
+            #     # control = FORESEE_CBF_QP_COV.controller(params, sigma_points[:,i].reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
+            #     z = jnp.append( sigma_points[:,i].reshape(-1,1), control, axis=0 )
             #     pred_mu, next_state_cov = self.gaussian_process.jax_predict(z=z)
+            #     # pdb.set_trace()
             #     next_state_cov_root = get_ut_cov_root_diagonal(next_state_cov)    
-            #     next_state_mu = sigma_points[:,i].reshape(-1,1) - x_eq + (A @ (sigma_points[:,i].reshape(-1,1) - x_eq) + B @ (u - u_eq))*dt + x_eq + pred_mu           
+            #     next_state_mu = sigma_points[:,i].reshape(-1,1) - x_eq + (A @ (sigma_points[:,i].reshape(-1,1) - x_eq) + B @ (control - u_eq))*dt + x_eq + pred_mu           
             #     # pdb.set_trace()            
             #     temp_points, temp_weights = generate_sigma_points_gaussian( next_state_mu, next_state_cov_root, jnp.zeros((n,1)), 1.0 )
+            #     pdb.set_trace()
             #     new_points = new_points.at[:,i].set( temp_points.reshape(-1,1, order='F')[:,0] )
             #     new_weights = new_weights.at[:,i].set( temp_weights.reshape(-1,1, order='F')[:,0] * weights[:,i] )   
             # return_points, return_weights = new_points, new_weights
 
             def body(i, inputs):
                 new_points, new_weights = inputs        
-                # u = FORESEE_CBF_QP_COV.controller(params, sigma_points[:,i].reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
-                z = jnp.append( sigma_points[:,i].reshape(-1,1), u, axis=0 )
+                # control = FORESEE_CBF_QP_COV.controller(params, sigma_points[:,i].reshape(-1,1), X_GOAL, consA, consb).reshape(-1,1)
+                z = jnp.append( sigma_points[:,i].reshape(-1,1), control, axis=0 )
                 pred_mu, next_state_cov = self.gaussian_process.jax_predict(z=z)
                 next_state_cov_root = get_ut_cov_root_diagonal(next_state_cov)    
-                next_state_mu = sigma_points[:,i].reshape(-1,1) - x_eq + (A @ (sigma_points[:,i].reshape(-1,1) - x_eq) + B @ (u - u_eq))*dt + x_eq + pred_mu                       
+                next_state_mu = sigma_points[:,i].reshape(-1,1) - x_eq + (A @ (sigma_points[:,i].reshape(-1,1) - x_eq) + B @ (control - u_eq))*dt + x_eq + pred_mu                       
                 temp_points, temp_weights = generate_sigma_points_gaussian( next_state_mu, next_state_cov_root, jnp.zeros((n,1)), 1.0 )
                 new_points = new_points.at[:,i].set( temp_points.reshape(-1,1, order='F')[:,0] )
                 new_weights = new_weights.at[:,i].set( temp_weights.reshape(-1,1, order='F')[:,0] * weights[:,i] )   
@@ -423,7 +425,7 @@ class FORESEE_CBF_QP_COV(MPC):
         mup, covp = self.gaussian_process.predict(z.T, return_pred=False)
         action = FORESEE_CBF_QP_COV.controller(self.params, obs.reshape(-1,1), x_goal, self.constraints.state_constraints[1].A, self.constraints.state_constraints[1].b)
         
-        
+        # pdb.set_trace()
         # Update parameters
         nx, nu = self.model.nx, self.model.nu
         T = self.T
@@ -439,7 +441,7 @@ class FORESEE_CBF_QP_COV(MPC):
         const = FORESEE_CBF_QP_COV.constraint_func( jnp.copy(self.params), obs.reshape(-1,1), A, B, x_eq, u_eq, x_goal, self.constraints.state_constraints[1].A, self.constraints.state_constraints[1].b )
         print(f"rewards: {reward}, gras:{grads}")
 
-        pdb.set_trace()
+        # pdb.set_trace()
 
         t0 = time.time()
         
@@ -495,8 +497,8 @@ class FORESEE_CBF_QP_COV(MPC):
             self.results_dict['state_horizon_cov'].append(state_covariances)
             self.results_dict['horizon_states'].append( mus )
         else:
-            self.results_dict['state_horizon_cov'].append(jnp.zeros((T+1, nx, nx)))
-            self.results_dict['horizon_states'].append( jnp.zeros((nx, T)) )
+            self.results_dict['state_horizon_cov'].append(np.zeros((T+1, nx, nx)))
+            self.results_dict['horizon_states'].append( np.zeros((nx, T+1)) )
 
 
                     

safe_control_gym/controllers/mpc/foresee_utils_jax.py
@@ -47,7 +47,7 @@ def get_mean_cov(sigma_points, weights):
 def get_ut_cov_root_diagonal(cov):
     # return jnp.zeros((4,4))
     offset = 0.000  # TODO: make sure not zero here
-    root_term = jnp.diag( jnp.diagonal(cov)+offset  )
+    root_term = jnp.diag( jnp.sqrt(jnp.diagonal(cov)+offset)  )
     return root_term
     
     #root0 = jnp.sqrt((offset+cov[0,0]))
@@ -77,15 +77,10 @@ def get_mean_cov_skew_kurt_for_generation( sigma_points, weights ):
     cov = jnp.diag(jnp.sum(centered_points**2 * weights[0], axis=1))
     
     skewness_temp = jnp.sum(centered_points**3 * weights[0], axis=1) #/ cov[0,0]**(3/2) # for scipy    
-    skewness = skewness_temp[0] / cov[0,0]**(3/2)
-    skewness = jnp.append(skewness, skewness_temp[1] / cov[1,1]**(3/2))
-    skewness = jnp.append(skewness, skewness_temp[2] / cov[2,2]**(3/2))
-    skewness = jnp.append(skewness, skewness_temp[3] / cov[3,3]**(3/2))
+    skewness = skewness_temp / jnp.diag(cov)**(3/2)
+
     kurt_temp = jnp.sum(centered_points**4 * weights[0], axis=1)# / cov[0,0]**(4/2)  # -3 # -3 for scipy
-    kurt = kurt_temp[0]/cov[0,0]**(4/2)
-    kurt = jnp.append(kurt, kurt_temp[1]/cov[1,1]**(4/2))
-    kurt = jnp.append(kurt, kurt_temp[2]/cov[2,2]**(4/2))
-    kurt = jnp.append(kurt, kurt_temp[3]/cov[3,3]**(4/2))
+    kurt = kurt_temp / jnp.diag(cov)**(4/2)
 
     return mu, cov, skewness.reshape(-1,1), kurt.reshape(-1,1)
 